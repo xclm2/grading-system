68f441ff5910a08b061cd729aeb5d40f5c32ac3f
Excell Bolo <excell@zeke.zeke>
2023-08-26 17:19:42 +0800

    Temporary fix for  build issue, composer install not properly working on docker container

--- build.sh
+++ build.sh
@@ -1,9 +1,28 @@
 #!bin/sh
+sudo chown $USER:$USER -R project
+cp project/.env.example project/.env
+mv project temp
+
+echo "MOVING TO TEMP"
+sleep 5
 
 docker-compose up -d
-echo "Waiting for docker to complete"
-sleep 10
-docker-compose exec grading php artisan migrate
+echo "Wait for docker build to finish"
+sleep 5
+
+sudo chown $USER:$USER -R project
+echo "Changing permissions"
+sleep 1
+
+cp -R temp/. project
+echo "Copying to project"
+sleep 5
+
+rm -rf temp
+
 docker-compose exec grading npm install
+docker-compose exec grading php artisan key:generate
+docker-compose exec grading php artisan migrate
 docker-compose exec grading npm run dev
+
 echo "For more info visit: https://hub.docker.com/r/bitnami/laravel/"
